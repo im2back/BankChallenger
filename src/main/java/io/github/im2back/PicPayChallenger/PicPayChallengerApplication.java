56aec00a9300e6d78a709271e2e8e75c9397e5d3
im2back <jeff.trabalho@outlook.com>
2024-05-26 22:04:05 -0300

    Project Created

---
+++ src/main/java/io/github/im2back/PicPayChallenger/PicPayChallengerApplication.java
@@ -0,0 +1,13 @@
+package io.github.im2back.PicPayChallenger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicPayChallengerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PicPayChallengerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/im2back/PicPayChallenger/PicPayChallengerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.im2back.PicPayChallenger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicPayChallengerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
