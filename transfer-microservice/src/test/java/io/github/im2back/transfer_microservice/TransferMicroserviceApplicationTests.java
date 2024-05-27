05173f74e94eb3a421d83031174bdd21e6f60d4a
im2back <jeff.trabalho@outlook.com>
2024-05-27 19:42:44 -0300

    Create transfer-microservice

---
+++ transfer-microservice/src/test/java/io/github/im2back/transfer_microservice/TransferMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.im2back.transfer_microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransferMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
