6ce8fddeb731f085728dcc102702b859b6f8c853
im2back <jeff.trabalho@outlook.com>
2024-05-27 18:03:55 -0300

    Update Application Name

--- src/main/java/io/github/im2back/PicPayChallenger/UserMicroserviceApplication.java
+++ src/main/java/io/github/im2back/PicPayChallenger/UserMicroserviceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PicPayChallengerApplication {
+public class UserMicroserviceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PicPayChallengerApplication.class, args);
+		SpringApplication.run(UserMicroserviceApplication.class, args);
 	}
 
 }
