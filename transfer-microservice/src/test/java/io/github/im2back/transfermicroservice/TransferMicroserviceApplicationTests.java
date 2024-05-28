c783386df036f918701f36d17c010c2600a80308
im2back <jeff.trabalho@outlook.com>
2024-05-28 15:29:54 -0300

    test: Transfer Microservice- hhtpclient Unit test Implemented

--- transfer-microservice/src/test/java/io/github/im2back/transfermicroservice/TransferMicroserviceApplicationTests.java
+++ transfer-microservice/src/test/java/io/github/im2back/transfermicroservice/TransferMicroserviceApplicationTests.java
@@ -1,4 +1,4 @@
-package io.github.im2back.transfer_microservice;
+package io.github.im2back.transfermicroservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
