b6a7dde6a8cf0e59a27bf7859074fac4aec417c5
im2back <jeff.trabalho@outlook.com>
2024-05-28 15:02:27 -0300

    test: Model and Validations - Finish Unit Tests

---
+++ user-ms/src/test/java/io/github/im2back/usermicroservice/model/entities/wallet/WalletTest.java
@@ -0,0 +1,50 @@
+package io.github.im2back.usermicroservice.model.entities.wallet;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.math.BigDecimal;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import io.github.im2back.usermicroservice.model.entities.user.User;
+import io.github.im2back.usermicroservice.util.UtilsTest;
+
+class WalletTest {
+
+	@Test
+	@DisplayName("Deveria deduzir o valor recebido como parametro da carteira")
+	void transfer() {
+		// ARRANGE
+		User user = UtilsTest.userComum;
+		user.getWallet().setBalance(new BigDecimal(100));	
+		BigDecimal value = new BigDecimal(10);
+		
+		// ACT
+		user.getWallet().transfer(value);
+		
+		// ASSERT
+		assertEquals(new BigDecimal(90), user.getWallet().getBalance());
+	}
+	@Test
+	@DisplayName("Deveria acrecentar o valor recebido como parametro da carteira")
+	void receiveTransfer() {
+		// ARRANGE
+		User user = UtilsTest.userComum;
+		user.getWallet().setBalance(new BigDecimal(100));	
+		BigDecimal value = new BigDecimal(10);
+		
+		// ACT
+		user.getWallet().receiveTransfer(value);
+		
+		// ASSERT
+		assertEquals(new BigDecimal(110), user.getWallet().getBalance());
+	}
+
+}
+
+
+
+
+
+
---
+++ user-ms/src/test/java/io/github/im2back/usermicroservice/validation/user/DocumentCannotBeDuplicatedTest.java
@@ -0,0 +1,59 @@
+package io.github.im2back.usermicroservice.validation.user;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.BDDMockito;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import io.github.im2back.usermicroservice.model.entities.user.User;
+import io.github.im2back.usermicroservice.repositories.UserRepository;
+import io.github.im2back.usermicroservice.service.exceptions.CannotBeDuplicatedException;
+import io.github.im2back.usermicroservice.util.UtilsTest;
+
+@ExtendWith(MockitoExtension.class)
+class DocumentCannotBeDuplicatedTest {
+	@Mock
+	private UserRepository userRepository;
+
+	@InjectMocks
+	private DocumentCannotBeDuplicated documentCannotBeDuplicatedTest;
+
+	@Test
+	@DisplayName("Deveria lançar uma exceção do tipo CannotBeDuplicatedException em caso de documento já utilizado por outro usuário")
+	void valid() {
+		// ARRANGE
+		Optional<User> user = Optional.ofNullable(UtilsTest.userComum);
+		BDDMockito
+				.when(userRepository
+						.findByIdentificationDocument(UtilsTest.userRegisterRequest.identificationDocument()))
+				.thenReturn(user);
+
+		// ACT + ASSERT
+		assertThrows(CannotBeDuplicatedException.class,
+				() -> documentCannotBeDuplicatedTest.valid(UtilsTest.userRegisterRequest));
+
+	}
+
+	@Test
+	@DisplayName("Não deveria lançar uma exceção do tipo CannotBeDuplicatedException em caso de documento não utilizado por outro usuário")
+	void shouldNotThrowExceptionWhenDocumentIsNotUsed() {
+		// ARRANGE
+		Optional<User> user = Optional.empty();
+		BDDMockito
+				.when(userRepository
+						.findByIdentificationDocument(UtilsTest.userRegisterRequest.identificationDocument()))
+				.thenReturn(user);
+
+		// ACT + ASSERT
+		assertDoesNotThrow(() -> documentCannotBeDuplicatedTest.valid(UtilsTest.userRegisterRequest));
+	}
+
+}
