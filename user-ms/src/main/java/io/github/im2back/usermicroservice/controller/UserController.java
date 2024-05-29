943178e51f6abe9bd1ce4729e08e20428fd8721b
im2back <jeff.trabalho@outlook.com>
2024-05-29 11:31:51 -0300

    doc: User Microservice - documentation Complete

--- user-ms/src/main/java/io/github/im2back/usermicroservice/controller/UserController.java
+++ user-ms/src/main/java/io/github/im2back/usermicroservice/controller/UserController.java
@@ -1,6 +1,7 @@
 package io.github.im2back.usermicroservice.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,11 +11,13 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
-
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.github.im2back.usermicroservice.model.dto.TransferRequestDto;
 import io.github.im2back.usermicroservice.model.dto.UserRegisterRequestDto;
 import io.github.im2back.usermicroservice.model.dto.UserRegisterResponseDto;
 import io.github.im2back.usermicroservice.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 
 @RequestMapping("/users")
@@ -24,6 +27,12 @@ public class UserController {
 	@Autowired
 	private UserService userService;
 
+	@Operation(summary = "Retorna um DTO de usuário apartir do ID informado no path")
+	@ApiResponses(value = { 
+			@ApiResponse(responseCode = "200", description = "Retorna UserRegisterResponseDto"),
+			@ApiResponse(responseCode = "400", description = "Retorna uma exceção em caso de ID não encontrado UserNotFoundException"),
+			@ApiResponse(responseCode = "500", description = "Erro interno no servidor")
+			})
 	@GetMapping(value = "/{id}")
 	ResponseEntity<UserRegisterResponseDto> findUser(@PathVariable Long id) {
 		var user = userService.findById(id);
@@ -31,7 +40,15 @@ public class UserController {
 				.ok(new UserRegisterResponseDto(user.getId(), user.getFullName(), user.getIdentificationDocument(),
 						user.getEmail(), user.getPassword(), user.getType(), user.getWallet().getBalance()));
 	}
-
+	
+	
+	@Operation(summary = "Retorna um DTO de usuário após cadastra-lo no banco de dados")
+	@ApiResponses(value = { 
+			@ApiResponse(responseCode = "201", description = "Retorna UserRegisterResponseDto"),
+			@ApiResponse(responseCode = "409", description = "Retorna uma exceção em caso de DOCUMENTO duplicado CannotBeDuplicatedException"),
+			@ApiResponse(responseCode = "409", description = "Retorna uma exceção em caso de EMAIL duplicado CannotBeDuplicatedException"),
+			@ApiResponse(responseCode = "500", description = "Erro interno no servidor")
+			})
 	@PostMapping
 	ResponseEntity<UserRegisterResponseDto> saveUser(@Valid @RequestBody UserRegisterRequestDto dto,
 			UriComponentsBuilder uriBuilder) {
@@ -39,7 +56,12 @@ public class UserController {
 		var uri = uriBuilder.path("/users/{id}").buildAndExpand(response.id()).toUri();
 		return ResponseEntity.created(uri).body(response);
 	}
-
+	
+	@Operation(summary = "Realiza uma operação de transferencia e não retorna corpo")
+	@ApiResponses(value = { 
+			@ApiResponse(responseCode = "200", description = "Retorna status 200 em caso de operação bem sucedida"),
+			@ApiResponse(responseCode = "500", description = "Erro interno no servidor")
+			})
 	@PutMapping("/transfer")
 	ResponseEntity<Void> transfer(@Valid @RequestBody TransferRequestDto dto) {
 
